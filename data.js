a5519ed12a536e9b499fec6a5d4c20a87b505bdc
LTPhat <102889533+LTPhat@users.noreply.github.com>
2023-03-17 23:33:56 +0700

    add modules

---
+++ data.js
@@ -0,0 +1,140 @@
+export const music_list = [
+    {
+        img : 'poster/ATBE.jpg',
+        name : 'Âm Thầm Bên Em',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/AmThamBenEm-SonTungMTP-4066476.mp3'
+    },
+    {
+        img : 'poster/ASR.jpg',
+        name : 'Anh Sai Rồi',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/AnhSaiRoi-MTP-2647024.mp3'
+    },
+    {
+        img : 'poster/BYNPG.jpg',
+        name : 'Bình Yên Những Phút Giây',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/BinhYenNhungPhutGiay-SonTungMTP-4915711.mp3'
+    },
+    {
+        img : 'poster/BYND.jpg',
+        name : 'Bình Yên Nơi Đâu',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/BinhYenNoiDau-M-TP_452gz.mp3'
+    },
+    {
+        img : 'poster/BDTNR.jpg',
+        name : 'Buông Đôi Tay Nhau Ra',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/BuongDoiTayNhauRa-SonTungMTP-4184408.mp3'
+    },
+    {
+        img : 'poster/CADSV.jpg',
+        name : 'Chắc Ai Đó Sẽ Về',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/ChacAiDoSeVeNewVersion-SonTungMTP-3698905.mp3'
+    },
+    {
+        img : 'poster/CTCHT.jpg',
+        name : 'Chúng Ta Của Hiện Tại',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/chungtacuahientai.mp3'
+    },
+    {
+        img : 'poster/CTKTVN.jpg',
+        name : 'Chúng Ta Không Thuộc Về Nhau',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/ChungTaKhongThuocVeNhau-SonTungMTP-4528181.mp3'
+    },
+    {
+        img : 'poster/CCYLD.jpg',
+        name : 'Có Chắc Yêu Là Đây',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/CoChacYeuLaDayOnionnRemix-SonTungMTPOnionn-7022615.mp3'
+    },
+    {
+        img : 'poster/CMXD.jpg',
+        name : 'Cơn Mưa Xa Dần',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/ConMuaXaDan-SonTungMTP-8033250.mp3'
+    },
+    {
+        img : 'poster/DVT.jpg',
+        name : 'Đừng Về Trễ',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/DungVeTreRnbVersion-MTP-2691584.mp3'
+    },
+    {
+        img : 'poster/HTCA.jpg',
+        name : 'Hãy Trao Cho Anh',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/HayTraoChoAnh-SonTungMTPSnoopDogg-6010660.mp3'
+    },
+    {
+        img : 'poster/LT.jpg',
+        name : 'Lạc Trôi',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/LacTroiTripleDRemix-SonTungMTP-5164670.mp3'
+    },
+    {
+        img : 'poster/MMCD.jpg',
+        name : 'Một Mình Cô Đơn',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/motminhcodon.mp3'
+    },
+    {
+        img : 'poster/MNMBA.jpg',
+        name : 'Một Năm Mới Bình An',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/MotNamMoiBinhAn-SonTungMTP-4315569.mp3'
+    },
+    {
+        img : 'poster/MRMSC.png',
+        name : 'Muộn Rồi Mà Sao Còn',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3'
+    },
+    {
+        img : 'poster/NANQ.jpg',
+        name : 'Nắng Ấm Ngang Qua',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/NangAmNgangQua-SonTungMTP-8033251.mp3'
+    },
+    {
+        img : 'poster/NNHQ.jpg',
+        name : 'Như Ngày Hôm Qua',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/NhuNgayHomQuaUpgrade-SonTungMTP-4282962.mp3'
+    },
+    {
+        img : 'poster/NNCA.jpg',
+        name : 'Nơi Này Có Anh',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/NoiNayCoAnh-SonTungMTP-4772041.mp3'
+    },
+    {
+        img : 'poster/RMBM.jpg',
+        name : 'Remember Me',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/rememberme.mp3'
+    },
+    {
+        img : 'poster/TNOAA.jpg',
+        name : 'There Is No One At All',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/TheresNoOneAtAll-SonTungMTP-7583837.mp3'
+    },
+    {
+        img : 'poster/TLVNO.jpg',
+        name : 'Tiến Lên Việt Nam Ơi',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/TienLenVietNamOi-SonTungMTP-4003040.mp3'
+    },
+    {
+        img : 'poster/YOYTD.jpg',
+        name : 'You Of Yesterday',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/YouOfYesterday-SonTungMTP-5524481.mp3'
+    }
+];
--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-let title = document.querySelector('.title');
-let now_playing = document.querySelector('.now-playing');
-let track_art = document.querySelector('.track-art');
-let track_name = document.querySelector('.track-name');
-let track_artist = document.querySelector('.track-artist');
-
-let playpause_btn = document.querySelector('.playpause-track');
-let next_btn = document.querySelector('.next-track');
-let prev_btn = document.querySelector('.prev-track');
-
-let seek_slider = document.querySelector('.seek-slider');
-let volume_slider = document.querySelector('.volume-slider');
-let curr_time = document.querySelector('.current-time');
-let total_duration = document.querySelector('.total-duration');
-let wave = document.getElementById('wave');
-let randomIcon = document.querySelector('.fa-random');
-let curr_track = document.createElement('audio');
-
-
-let track_index = 0;
-let isPlaying = false;
-let isRandom = false;
-let updateTimer;
-let bgTimer;
-
 const music_list = [
     {
         img : 'poster/ATBE.jpg',
@@ -31,10 +6,28 @@ const music_list = [
         music : 'music/AmThamBenEm-SonTungMTP-4066476.mp3'
     },
     {
+        img : 'poster/ASR.jpg',
+        name : 'Anh Sai Rồi',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/AnhSaiRoi-MTP-2647024.mp3'
+    },
+    {
+        img : 'poster/BYNPG.jpg',
+        name : 'Bình Yên Những Phút Giây',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/BinhYenNhungPhutGiay-SonTungMTP-4915711.mp3'
+    },
+    {
         img : 'poster/BYND.jpg',
         name : 'Bình Yên Nơi Đâu',
         artist : 'Sơn Tùng M-TP',
         music : 'music/BinhYenNoiDau-M-TP_452gz.mp3'
+    },
+    {
+        img : 'poster/BDTNR.jpg',
+        name : 'Buông Đôi Tay Nhau Ra',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/BuongDoiTayNhauRa-SonTungMTP-4184408.mp3'
     },
     {
         img : 'poster/CADSV.jpg',
@@ -43,10 +36,22 @@ const music_list = [
         music : 'music/ChacAiDoSeVeNewVersion-SonTungMTP-3698905.mp3'
     },
     {
+        img : 'poster/CTCHT.jpg',
+        name : 'Chúng Ta Của Hiện Tại',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/chungtacuahientai.mp3'
+    },
+    {
         img : 'poster/CTKTVN.jpg',
         name : 'Chúng Ta Không Thuộc Về Nhau',
         artist : 'Sơn Tùng M-TP',
         music : 'music/ChungTaKhongThuocVeNhau-SonTungMTP-4528181.mp3'
+    },
+    {
+        img : 'poster/CCYLD.jpg',
+        name : 'Có Chắc Yêu Là Đây',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/CoChacYeuLaDayOnionnRemix-SonTungMTPOnionn-7022615.mp3'
     },
     {
         img : 'poster/CMXD.jpg',
@@ -71,6 +76,12 @@ const music_list = [
         name : 'Lạc Trôi',
         artist : 'Sơn Tùng M-TP',
         music : 'music/LacTroiTripleDRemix-SonTungMTP-5164670.mp3'
+    },
+    {
+        img : 'poster/MMCD.jpg',
+        name : 'Một Mình Cô Đơn',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/motminhcodon.mp3'
     },
     {
         img : 'poster/MNMBA.jpg',
@@ -103,6 +114,18 @@ const music_list = [
         music : 'music/NoiNayCoAnh-SonTungMTP-4772041.mp3'
     },
     {
+        img : 'poster/RMBM.jpg',
+        name : 'Remember Me',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/rememberme.mp3'
+    },
+    {
+        img : 'poster/TNOAA.jpg',
+        name : 'There Is No One At All',
+        artist : 'Sơn Tùng M-TP',
+        music : 'music/TheresNoOneAtAll-SonTungMTP-7583837.mp3'
+    },
+    {
         img : 'poster/TLVNO.jpg',
         name : 'Tiến Lên Việt Nam Ơi',
         artist : 'Sơn Tùng M-TP',
@@ -116,10 +139,37 @@ const music_list = [
     }
 ];
 
-// Load random gradient background color
+
+let now_playing = document.querySelector('.now-playing');
+let track_art = document.querySelector('.track-art');
+let track_name = document.querySelector('.track-name');
+let track_artist = document.querySelector('.track-artist');
+
+
+let playpause_btn = document.querySelector('.playpause-track');
+let next_btn = document.querySelector('.next-track');
+let prev_btn = document.querySelector('.prev-track');
+let wrapper = document.querySelector('.wrapper');
+let seek_slider = document.querySelector('.seek-slider');
+let volume_slider = document.querySelector('.volume-slider');
+let curr_time = document.querySelector('.current-time');
+let total_duration = document.querySelector('.total-duration');
+let wave = document.getElementById('wave');
+let randomIcon = document.querySelector('.fa-random');
+let curr_track = document.createElement('audio');
+let stroke = document.getElementsByClassName('stroke');
+
+let playingFirst = true;
+let track_index = 0;
+let isPlaying = true;
+let isRandom = false;
+let updateTimer;
+let bgTimer;
+let strokeTimer;
+
+// Function to change color
+
 function load_bg(){
-    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
-    // Function to create random hex_color
     function random_color(hex_code){
         for (let i = 0; i < 6; i++){
             let index = Math.round(Math.random() * 14);
@@ -128,33 +178,121 @@ function load_bg(){
         }
         return hex_code;
     }
+    let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
     let first_color = random_color("#");
     let second_color = random_color("#");
     var angle = 'to right';
+    var inverse_angle = 'to left';
     let gradient = 'linear-gradient('+ angle + ',' + first_color + ',' + second_color +')';
+    let warpper_gradient = 'linear-gradient('+ inverse_angle + ',' + first_color + ',' + second_color +')';
     document.body.style.background = gradient;
+    wrapper.style.background = warpper_gradient;
 }
 
-// Function to load a song
 
-function load_track(track_index){
+function changeStroke_color(){
+    main_color = ['#09e811', '#e83fd4', '#cf0808','#575151','#14dbdb', '#6f07e6', '#0261e6', '#c4e009', '#0e0f0e', '#d0ff00']
+    add_color = ['#05850a', '#612159','#7d0505','#a19c9c', '#1c8787', '#34026e', '#063e8c', '#627004', '#778077', '#6d8505']
+    let index = Math.round(Math.random() * (main_color.length));
+    let first_color = main_color[index];
+    let second_color = add_color[index];
+    let angle = 'to bottom';
+    let to_bottom_gradient = 'linear-gradient('+ angle + ',' + first_color + ',' + second_color +')';
+    for (let i = 0; i < 7; i++){
+        stroke[i].style.animation = 'animate 1.4s linear infinite';
+        stroke[i].style.animationDelay = timeDelay[i] + 's';
+        stroke[i].style.background = to_bottom_gradient;
+    }
+}
+
+// Load song
+
+function loadTrack(track_index){
+    if (track_index > music_list.length - 1){
+        alert("Out of index");
+    }
     clearInterval(updateTimer);
     reset();
 
     curr_track.src = music_list[track_index].music;
     curr_track.load();
 
-    track_art.style.backgroundImage = "url("+ music_list[track_index].img+")";
+    track_art.style.backgroundImage = "url("+ music_list[track_index].img + ")";
     track_name.textContent = music_list[track_index].name;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    now_playing.textContent = "Playing track " + (track_index + 1) + " of " + music_list.length;
 
     // Update time
-    updateTimer = setInterval(setUpdate, 1000);
-    curr_track.addEventListener('ended', nextTrack);
+    updateTimer = setInterval(setUpdate, 100);
+    // curr_track.addEventListener('ended', nextTrack);
+    load_bg();
     bgTimer = setInterval(load_bg, 1000);
     clearInterval(bgTimer);
     bgTimer = setInterval(load_bg, 60000);
 }
+
+// Play song
+
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+    track_art.classList.add('rotate');
+    wave.classList.add('loader');
+    timeDelay = [0, 0.3, 0.6, 0.9, 0.6, 0.3, 0];
+    // let angle = 'to right'
+    playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-4x"></i>';
+}
+
+
+// Function to change time and volume
+
+function seekto(){
+    clearInterval(updateTimer);
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+    let currMinutes = Math.floor(curr_track.currentTime / 60);
+    let currSeconds = Math.floor(curr_track.currentTime - 60 * currMinutes)
+    curr_time.textContent = currMinutes + ":" + currSeconds;
+    updateTimer = setInterval(setUpdate, 100);
+}
+
+function set_volume(){
+    curr_track.volume = volume_slider.value / 100;
+}
+
+
+// Play/Pause
+
+function playpauseTrack(){
+    if (isPlaying && playingFirst){
+        clearInterval(strokeTimer);
+        playingFirst = false;
+        pauseTrack();
+    }
+    if (isPlaying){
+        clearInterval(strokeTimer);
+        pauseTrack();
+    }
+    else{
+        changeStroke_color()
+        strokeTimer = setInterval(changeStroke_color, 3000);
+        playTrack();
+    }
+}
+
+
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    for (let i = 0; i < 7; i++){
+        stroke[i].style.animation = 'none';
+        stroke[i].style.background = 'black';
+    }
+    track_art.classList.remove('rotate');
+    playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-4x"></i>'
+}
+
+
+
 function reset(){
     curr_time.textContent = "00:00";
     total_duration.textContent = "00:00";
@@ -162,13 +300,56 @@ function reset(){
 }
 
 
-// Function to change time and volume
+// Random
 
-function seekto(){
-    let seekto = curr_track.duration * (seek_slider.value / 100);
-    curr_track.duration = seekto;
+function playRandom(){
+    isRandom = true;
+    randomIcon.classList.add('randomActive');
 }
 
-function set_volume(){
-    curr_track.volume = volume_slider.value / 100;
+function pauseRandom(){
+    isRandom = false;
+    randomIcon.classList.remove('randomActive');
 }
+
+function repeatTrack(){
+    let curr_track = track_index;
+    playTrack();
+}
+
+
+
+function setUpdate(){
+    let position = 0;
+    if (!isNaN(curr_track.duration)){
+        position = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = position;
+
+        let currMinutes = Math.floor(curr_track.currentTime / 60);
+        let currSeconds = Math.floor(curr_track.currentTime - 60 * currMinutes)
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - 60 * durationMinutes);
+
+        // Padding with 0
+        if (currMinutes < 10) {
+            currMinutes = "0" + currMinutes;
+        }
+        if (currSeconds < 10){
+            currSeconds =  "0" + currSeconds;
+        }
+        if (durationMinutes < 10){
+            durationMinutes = "0" + durationMinutes;
+        }
+        if (durationSeconds < 10){
+            durationSeconds = "0" + durationSeconds;
+        }
+        curr_time.textContent = currMinutes + ':' + currSeconds;
+        total_duration.textContent = durationMinutes + ':' + durationSeconds;
+    }
+}
+
+// test
+loadTrack(20)
+playTrack();
+changeStroke_color();
+strokeTimer = setInterval(changeStroke_color, 3000);
